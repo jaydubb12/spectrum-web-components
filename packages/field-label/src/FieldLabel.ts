d0e6c72c19dbe9c31171f01fc5d74777b0aa1284
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-09-07 17:53:37 -0400

    feat(field-label): add field-label pattern

---
+++ packages/field-label/src/FieldLabel.ts
@@ -0,0 +1,42 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import {
+    html,
+    SpectrumElement,
+    CSSResultArray,
+    TemplateResult,
+    property,
+} from '@spectrum-web-components/base';
+
+import styles from './field-label.css.js';
+
+/**
+ * @element sp-field-label
+ */
+export class FieldLabel extends SpectrumElement {
+    public static get styles(): CSSResultArray {
+        return [styles];
+    }
+
+    @property({ type: Boolean, reflect: true })
+    public disabled = false;
+
+    @property({ type: String, attribute: 'inline-alignment', reflect: true })
+    public inlineArgument?: 'left' | 'right';
+
+    protected render(): TemplateResult {
+        return html`
+            <label><slot></slot></label>
+        `;
+    }
+}
