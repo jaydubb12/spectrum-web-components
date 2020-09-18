d83cc5caa86c6196b78b6655690aafd726745d1f
Westbrook Johnson <westbrook.johnson@gmail.com>
2020-09-18 13:51:47 -0400

    docs: build them with 11ty, wip

--- test/visual/screenshots-baseline/regenerate.js
+++ test/visual/screenshots-baseline/regenerate.js
@@ -33,6 +33,7 @@ module.exports = {
                         appIndex: 'index.html',
                         rootDir: path.resolve(
                             process.cwd(),
+                            'projects',
                             'documentation',
                             'dist',
                             'storybook'
